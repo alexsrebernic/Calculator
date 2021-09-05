2d1d3c9708db569c4d11bd6a164a16f578b57ad3
Alex Srebernic <alexsrebernic@pop-os.localdomain>
2021-09-05 19:39:41 -0300

    add function checkCharsOfString, clean Delete Char function, add function checkChards to addChards function, remove old simbol operators to normal simbols

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const displayNumbers = document.querySelector("#showNumbers");
 const displayResult = document.querySelector("#result");
 const p = document.getElementById("para")
@@ -10,11 +11,15 @@ const numbers = document.querySelectorAll(".number")
 const operators = document.querySelectorAll(".operator")
 const undo = document.querySelector("#undo")
 const escapeButton = document.querySelector("#escape")
-console.log(operators)
-coma.onclick = () => displayCalculate()
+
+equal.onclick = () => 
+coma.onclick = () => displayCalculate(coma)
 undo.onclick = () =>  deleteChar()
 escapeButton.onclick = () => clearDisplayNumber()
 window.addEventListener("keydown",handleKeyboardInput)
+parentesis.forEach(function(parentesis){
+    parentesis.onclick = () => displayCalculate(parentesis)
+})
 numbers.forEach(function(number){
     number.onclick = () => displayCalculate(number)
 })
@@ -23,77 +28,90 @@ operators.forEach(function(operator){
 })
 
 function handleKeyboardInput(e) {
-    if (e.key >= 0 && e.key <= 9) displayCalculate(e.key)
+    if (e.key>= 0 && e.key <= 9) displayCalculate(e.key)
     if (e.key === ',') displayCalculate()
     if (e.key === '=' || e.key === 'Enter') evaluate()
     if (e.key === 'Backspace') deleteChar()
-    if (e.key === 'Escape') displayCalculate()
+    if (e.key === 'Escape') clearDisplayNumber()
     if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
-      displayCalculate(convertOperator(e.key))
+      displayCalculate(e.key)
   }
-  function convertOperator(keyboardOperator) {
-    if (keyboardOperator === '/') return '÷'
-    if (keyboardOperator === '*') return '×'
-    if (keyboardOperator === '-') return '−'
-    if (keyboardOperator === '+') return '+'
-  }
-
-
-  
 
 function displayCalculate(value){  
-    if (value.textContent === "χ²"){
-        value.textContent = "^2"
-     }
-     if(value.className == "operator" && p.textContent.substring(p.textContent.length - 1) == "+")  return
-     if(value.className == "operator" && p.textContent.substring(p.textContent.length - 1) == "−")  return
-     if(value.className == "operator" && p.textContent.substring(p.textContent.length - 1) == "×")  return
-     if(value.className == "operator" && p.textContent.substring(p.textContent.length - 1) == "÷")  return
-     if(value.className == "operator" && p.textContent.substring(p.textContent.length - 1) == "√()")  return
-     if(value.className == "operator" && p.textContent.substring(p.textContent.length - 1) == "^2")  return
+checkIfHasParentesis()
 
-
-    makePara(p)
+    if(checkOperators(value)) return
+    makeParagraph()
     addChars(value)  
     
 }
+function checkIfHasParentesis(value){
+    if(p.textContent.includes("()")){
+        console.log("ASD")
+      let indexOfParentesis = p.textContent.indexOf("(")
+      let hasOpenParentesis = true
+      p.textContent[indexOfParentesis]
+    }
+}
 
-function makePara (para){
+function checkOperators(value){
+     if(value.className == "operator" && p.textContent.substring(p.textContent.length - 1) == "+")  return true
+     if(value.className == "operator" && p.textContent.substring(p.textContent.length - 1) == "-")  return true
+     if(value.className == "operator" && p.textContent.substring(p.textContent.length - 1) == "*")  return true
+     if(value.className == "operator" && p.textContent.substring(p.textContent.length - 1) == "/")  return true
+     if(value.className == "operator" && p.textContent.substring(p.textContent.length - 1) == "(")  return true
+     if(value.className == "operator" && p.textContent.substring(p.textContent.length - 1) == "%")  return true
+     if(value.className == "operator" && p.textContent.substring(p.textContent.length - 1) == ")")  return true
+     if(value.className == "operator" && p.textContent.substring(p.textContent.length - 2) == "**")  return true
+}
+
+function makeParagraph (){
     if (displayNumbers.innerHTML == ""){   
-        displayNumbers.appendChild(para)
-       para.style.height = "20px"
-       para.style.width = "95%"      
+        displayNumbers.appendChild(p)
+       p.style.height = "20px"
+       p.style.width = "95%"      
     }
 }
 
 function addChars (value){
     if (p.textContent.length < 33){
-        if(value.className == "operator" && p.textContent == "") {
-            return
-        } else {
-            toString(para.textContent += value.textContent)
-        }
-
-       
+        checkChars(value)
+        putLimitNumbers(para)
+     }
     } 
-    limitNumbers(para)
+
+function checkChars (value){
+    if (value.textContent == "("){
+        return toString(para.textContent += "()")
+     }
+     if(value.textContent == ")"){
+
+     }
+     if(value.className == "operator" && p.textContent == "") {
+         return
+     } else {
+         toString(para.textContent += value.textContent)
+     }
+}
+function deleteChar(){
+    checkCharsOfString()
+    putLimitNumbers(p)    
 }
 
-function deleteChar(){
-    let para = p.innerHTML
-    if(para.slice(-2) == "^2"){
-       
-        p.textContent = para.slice(0, -2)
-    }
-    p.textContent = para.slice(0,-1)
-    limitNumbers(p)    
+function checkCharsOfString(){
+    if(p.textContent.substr(p.textContent.length - 2) === "**" || p.textContent.substr(p.textContent.length - 2) === "()"){
+        p.textContent = p.textContent.slice(0,-2)
+        } else {
+        p.textContent = p.textContent.slice(0,-1)
+        }
 }
 
 function clearDisplayNumber(){
     p.textContent = ""
+   span.style.display = "none"
 }
 
-function limitNumbers(p){
+function putLimitNumbers(p){
     if(p.textContent.length >= 33){
         span.style.display = "block"
     } else if (p.textContent.length < 33){
@@ -102,45 +120,7 @@ function limitNumbers(p){
 }
 
 
-function add (a,b){
-    return a + b
-}
-function substract (a,b){
-    return a - b
-}
-function multiply (a,b){
-    return a * b
-}
-function divide (a,b){
-    return a / b
-}
-function potencia (a){
-    return Math.pow(a)
-}
-function squareRoot(a){
-    return Math.sqrt(a)
-}
 
-function operate(operator, a, b) {
-    a = Number(a)
-    b = Number(b)
-    switch (operator) {
-      case '+':
-        return add(a, b)
-      case '−':
-        return substract(a, b)
-      case '×':
-        return multiply(a, b)
-      case 'χ²':
-        return potencia(a)
-      case '√()':
-        return squareRoot(a)
-      case '÷':
-        if (b === 0) return null
-        else return divide(a, b)
-      default:
-        return null
-    }
-}
+
 
 
